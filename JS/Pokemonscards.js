1f469fbb8e41fdcebb03aa8888b1c2ef7895641e
Malvaez26 <daniel.malvaezh@gmail.com>
2024-01-17 14:06:19 -0600

    Se modifico archivo JS agregabdo un getPokemonData y const showPokemonCard, dentro de ello se crearon const id, const name y const weight para el peso. Se agrego un const image esperando nos devuelva la imagen del pokemon.

--- JS/Pokemonscards.js
+++ JS/Pokemonscards.js
@@ -11,6 +11,39 @@ const fetchPokemon = async () => {
     }
 }
 
+const getPokemonData = async (pokemonUrl) => {
+    try {
+        const response = await fetch(pokemonUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error al obtener datos del Pokémon:', error);
+    }
+};
 
+const showPokemonCard = (pokemon) => {
+    const card = document.createElement('div');
+    card.classList.add('pokemon-card');
 
+    const name = document.createElement('p');
+    name.textContent = `Nombre: ${pokemon.name}`;
+
+    const id = document.createElement('p');
+    id.textContent = `ID: ${pokemon.id}`;
+
+    const weight = document.createElement('p');
+    weight.textContent = `Peso: ${pokemon.weight} kg`;
+
+    const image = document.createElement('img');
+    image.src = pokemon.sprites.front_default;
+    image.alt = pokemon.name;
+    image.classList.add('pokemon-img');
+
+    card.appendChild(name);
+    card.appendChild(id);
+    card.appendChild(weight);
+    card.appendChild(image);
+
+    cardsContainer.appendChild(card);
+};
 
