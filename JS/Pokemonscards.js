f08e4433ba39fe5ea6427e8b4f3c936e27b1aa8b
Malvaez26 <daniel.malvaezh@gmail.com>
2024-01-17 13:57:56 -0600

    Se agregron las primeras varibales para resolver el ejercicio. Se agrego const API_URL y const cardContainer, utilizando get.ElementByID.

--- JS/Pokemonscards.js
+++ JS/Pokemonscards.js
@@ -1,4 +1,16 @@
 const API_URL = `https://pokeapi.co/api/v2`;
 const cardsContainer = document.getElementById('cards-container');
 
+const fetchPokemon = async () => {
+    try {
+        const data = await fetch(`${API_URL}/pokemon?limit=151`);
+        const parsedData = await data.json();
+        return parsedData.results;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
 
